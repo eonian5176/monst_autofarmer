13f6befc0a7857997aee71f01664fef760cfbc01
Kevin Shen <kevin.shen@symetra.com>
2024-07-20 03:48:17 -0700

    added masamune2 ball art tracking

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import pandas as pd
 device = ADBUtils.from_device()
 device.take_screenshot(Path(os.getcwd()), "monst_battle")
 
-unit = "chihaya"
+unit = "masamune2"
 
 unit_centers = pd.read_csv("ball art/centers.csv", dtype={"unit": "string", "x": "int32", "y": "int32"}).set_index("unit")
 
 # device.swipe_relative((0.3, 0.3), (0.5, 0.5))
-start_pt = template_match(image_path="test case/in bubble.png", template_path=f"ball art/{unit}.png", threshold=0.7)
+start_pt = template_match(image_path="monst_battle.png", template_path=f"ball art/{unit}.png", threshold=0.5)
 center_offset_data = unit_centers.loc[unit]
 unit_coords = (start_pt[1] + center_offset_data["x"], start_pt[0] + center_offset_data["y"])
 print(unit_coords)
